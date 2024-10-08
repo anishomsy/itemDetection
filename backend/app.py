1c4a4640dd6bb300a0a6736608f1b3faf0aeb05d
Anis Homsy <anisfhomsi@gmail.com>
2024-10-08 12:33:02 +0300

    added GA to .gitignore

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,22 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
 
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 with app.app_context():
     db.create_all()
 
 
-@app.route('/test', methods=['GET'])
+@app.route("/test", methods=["GET"])
 def test():
     return jsonify({"message": "Hello World!"})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=4000)
+
